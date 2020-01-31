d04c1ca5c63e393a30cbc64fbd72b0b705cac63c
GRUBCAVE-WIN\Pharylon <me@zachshuford.com>
2020-01-30 20:53:23 -0500

    Added welcome to home screen

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,6 +7,7 @@ const Home: React.FC = () => {
   return (
     <div id="landing">
       <div>
+      <Link to="/welcome"><h1 className="main-link">Welcome</h1></Link>
         <Link to="/history"><h1 className="main-link">History</h1></Link>
         <Link to="/religion"><h1 className="main-link">Religion</h1></Link>
         <Link to="/places"><h1 className="main-link">Places</h1></Link>
